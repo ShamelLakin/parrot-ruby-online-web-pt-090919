62b44675fc62655a744e210ffd1e27ba1d0d3792
Shamel Lakin <christlife4u@gmail.com>
2019-09-10 20:25:48 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,9 @@
 def parrot(sound = "Squawk")
   puts "I'm a parrot hear me #{sound} !"
+  "Squawk"
 end
 
- parrot
+ 
 
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
